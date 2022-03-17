4b6d6da4859b9c2c11550bc68249276c43330ba2
Seyoung Hong <maiorem00@gmail.com>
2022-03-17 15:02:17 +0900

    Update App.vue

--- la-platform-ui/src/App.vue
+++ la-platform-ui/src/App.vue
@@ -9,7 +9,7 @@
 </template>
 
 <style lang="scss">
-@import 'bootstrap/scss/bootstrap';
+@import "bootstrap/scss/bootstrap";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
